09f30bd2c2b57959ef5ddb22fa1e0a1cf42f7987
Ramses ndame <ramsesndame34@gmail.com>
2023-11-04 14:16:06 +0100

    Feat: Demo section
    start creating the demo section such as the input that would help us to send the url to the AI api to get the summary article
    
    Reviewed-by: Victor
    
    ref: #3

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -1,11 +1,29 @@
 import asset from '../assets'
-const Demo = () => {
+import {useState} from "react";
 
-    return(
+interface Article {
+    url: string,
+    summary: string
+}
+
+const Demo = () => {
+    const [article, setArticle] = useState<Article>({
+        url: '',
+        summary: ''
+    })
+    const handleSubmit = async (e:unknown) =>{
+        console.log(e)
+        alert("submit")
+    }
+    return (
         <section className="w-full mt-16 max-w-xl">
             <div className="flex flex-col w-full gap-2">
-                <form onSubmit={() =>{}} className="relative felx justify-center items-center">
-                    <img src={asset.linkIcon} alt="" className="absolute"/>
+                <form onSubmit={handleSubmit} className="relative felx justify-center items-center">
+                    <img src={asset.linkIcon} alt="link_icon" className="absolute left-0 my-2 ml-3 w-5"/>
+                    <input type="url" placeholder="Enter a URL" onChange={(e) => setArticle({...article,url:e.target.value})} value={article.url} required className="url_input peer"/>
+                    <button type="submit" className="submit_btn peer-focus:boder-gray-700 peer-focus:text-gray-700">
+                        ⏎
+                    </button>
                 </form>
 
             </div>
